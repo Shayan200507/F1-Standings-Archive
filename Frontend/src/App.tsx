9b7a109bcf0ab303891b6730a7c80449e81817ec
Shayan Sheikh <shayan.faisal1095@gmail.com>
2026-05-27 18:53:44 -0400

    Frontend header done

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import {Header} from "./Components/Header.tsx"
+
+function App() {
+
+
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/Components/Header.tsx
@@ -0,0 +1,39 @@
+
+import type {JSX} from "react"
+import logo from "../assets/formula-archive-logo.svg"
+
+
+export function Header(): JSX.Element{
+
+     return(
+
+       <>
+       
+       
+            <header className="header">
+                
+                
+                <img src={logo}></img>
+                <h1>F1 STANDINGS ARCHIVE</h1>
+                
+
+
+
+
+            </header>
+       
+       
+       
+      </>
+
+
+
+
+
+     )
+
+
+
+
+
+}
